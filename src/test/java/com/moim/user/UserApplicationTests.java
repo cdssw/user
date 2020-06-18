6cc21beb98d50951119d8a9fe9637e178b07adaa
cdssw <cdssw@DESKTOP-UKB1RQC>
2020-06-18 19:10:32 +0900

    profile 추가

--- src/test/java/com/moim/user/UserApplicationTests.java
+++ src/test/java/com/moim/user/UserApplicationTests.java
@@ -2,6 +2,7 @@ package com.moim.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
